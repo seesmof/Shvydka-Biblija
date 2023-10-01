e438c50b913d01086b4850523555fff630b605df
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-01 20:18:37 +0300

    update background.js and manifest.json

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.commands.onCommand.addListener(function (command) {
-  if (command === "open-website") {
+  if (command === "open-chat-gpt") {
     chrome.tabs.create({ url: "https://chat.openai.com/" });
+  }
+  if (command === "open-bing-chat") {
+    chrome.tabs.create({
+      url: "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx&wlexpsignin=1",
+    });
   }
 });
