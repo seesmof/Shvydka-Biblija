630bb649d2d2476aa84b02099508c7155867c086
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-24 23:17:37 +0300

    feat: update script files ✝️💓 praise Jesus Christ our Holy Lord GOD Almighty

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 chrome.commands.onCommand.addListener((command) => {
   const commandsMap = {
-    "open-chat-gpt": "https://chat.openai.com/",
     "open-bing-chat":
-      "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx&wlexpsignin=1",
+      "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx&wlexpsignin=1&showntbk=1",
+    "open-phind": "https://www.phind.com/agent",
+    "open-Blue-Letter-Bible": "https://www.blueletterbible.org/",
+    "open-Bible-Hub": "https://biblehub.com/bsb/",
   };
 
   if (commandsMap[command]) {
