01caae41539b36818cbc16c271e3ce89d88ce55d
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-08-09 18:21:15 +0300

    feat: HALLELUJAH THANK YOU JESUS CHRIST our HOLY LORD GOD ALMIGHTY KING OF KINGS AND LORD OF LORDS

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.commands.onCommand.addListener((command) => {
   const commandsMap = {
-    "open-bing-chat":
-      "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx&wlexpsignin=1&showntbk=1",
-    "open-phind": "https://www.phind.com/agent",
-    "open-Blue-Letter-Bible": "https://www.blueletterbible.org/",
-    "open-Bible-Hub": "https://biblehub.com/bsb/",
+    BlueLetterBIBLE: "https://www.blueletterbible.org/",
+    BIBLEhub: "https://biblehub.com/bsb/",
+    perplexity: "https://www.perplexity.ai/",
+    phind: "https://www.phind.com/agent",
   };
 
   if (commandsMap[command]) {
