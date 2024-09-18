1d3620c42c8949f676bfe917d220c40975c864be
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-18 10:35:18 +0300

    JESUS THANK YOU JESUS HALLELUJAH

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.commands.onCommand.addListener((command) => {
   const commandsMap = {
-    BlueLetterBIBLE: "https://www.blueletterbible.org/",
+    BlueLetterBIBLE: "https://www.blueletterbible.org/study/hta/hta.cfm#s=1",
     gemini: "https://gemini.google.com/app",
     bolls: "https://bolls.life/",
     phind: "https://www.phind.com/agent",
