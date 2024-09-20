7e11c91041681e16c6c88f02a0fc2a7f5b990b7f
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-20 14:17:26 +0300

    feat: JESUS kill me please i beg YOU JESUS please!

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
 chrome.commands.onCommand.addListener((command) => {
   const commandsMap = {
     BlueLetterBIBLE: "https://www.blueletterbible.org/study/hta/hta.cfm#s=1",
-    gemini: "https://gemini.google.com/app",
     bolls: "https://bolls.life/",
-    phind: "https://www.phind.com/agent",
   };
 
   if (commandsMap[command]) {
