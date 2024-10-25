ba3e913853b9bc56a761d9d4b2624089df0e5de3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-25 13:00:55 +0300

    feat: add eBible on Alt+Z, JESUS THANK YOU LORD GOD MOST HIGH

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ chrome.commands.onCommand.addListener((command) => {
   const commandsMap = {
     YouVersion:
       "https://www.bible.com/uk/users/OlegOnyshchenko504/reading-plans",
-    BollsLife: "https://bolls.life/profile/",
+    BollsLife: "https://bolls.life/",
+    eBible: "https://ebible.org/study/",
   };
 
   if (commandsMap[command]) {
