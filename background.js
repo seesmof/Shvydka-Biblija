3d34168d17ce52238eab2412b457b08930eae6c6
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-28 08:20:38 +0200

    feat: add Pastors' Study Bible to the list

--- background.js
+++ background.js
@@ -4,6 +4,7 @@ chrome.commands.onCommand.addListener((command) => {
       "https://www.bible.com/uk/users/OlegOnyshchenko504/reading-plans",
     BollsLife: "https://bolls.life/",
     eBible: "https://ebible.org/study/",
+    studyBible: "https://media.ipsapps.org/eng/osa/bible/gm-pastors/",
   };
 
   if (commandsMap[command]) {
