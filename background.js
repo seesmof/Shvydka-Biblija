a91d40477f0e7ef6598324c0a0d1ad926ad6f9c2
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-28 15:57:52 +0200

    add DailyVerse to shortcuts, JESUS THANK YOU LORD GOD ALMIGHTY ALLELUJAH AMEN

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.commands.onCommand.addListener((commandName) => {
   const commandNameToUrl = {
     Bolls: "https://bolls.life/",
     eBible: "https://ebible.org/study/",
+    DailyVerse: "https://dailyverses.net/random-bible-verse/nkjv",
   };
   chrome.tabs.create({ url: commandNameToUrl?.[commandName] });
 });
