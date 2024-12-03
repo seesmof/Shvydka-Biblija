b2460dc1bcb571ce62120b563325c360502e1724
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-03 17:35:10 +0200

    AMEN

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.commands.onCommand.addListener((commandName) => {
   const commandNameToUrl = {
     eBible: "https://ebible.org/study/",
     BlueLetterBible: "https://www.blueletterbible.org/",
+    YouVersion: "https://www.bible.com/users/OlegOnyshchenko504/reading-plans",
   };
   chrome.tabs.create({ url: commandNameToUrl?.[commandName] });
 });
