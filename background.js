26bf34f8a3cd874ff756b5f2deabf29b8774868f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-06 12:33:32 +0200

    AMEN

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.commands.onCommand.addListener((commandName) => {
     eBible: "https://ebible.org/study/",
     BlueLetterBible: "https://www.blueletterbible.org/",
     YouVersion: "https://www.bible.com/users/OlegOnyshchenko504/reading-plans",
+    BibleHub: "https://biblehub.com/bsb/",
   };
   chrome.tabs.create({ url: commandNameToUrl?.[commandName] });
 });
