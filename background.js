726d288b75a10457df2e2777014a9b7f118c2aa3
Oleh Onyshchenko <seesmwork@gmail.com>
2025-01-07 08:38:52 +0200

    AMEN

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener((commandName) => {
   const commandNameToUrl = {
     eBible: "https://ebible.org/study/",
     BlueLetterBible: "https://www.blueletterbible.org/",
-    YouVersion: "https://www.bible.com/users/OlegOnyshchenko504/reading-plans",
+    TSK: "https://tsk-online.com/TSK/",
     BollsLife: "https://bolls.life/",
   };
   chrome.tabs.create({ url: commandNameToUrl?.[commandName] });
