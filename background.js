7352fcd6adaa38b108ef843d899cedfe0e8f5027
Oleh Onyshchenko <seesmwork@gmail.com>
2025-02-05 09:51:10 +0200

    replace TSK with Instant.Bible

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener((commandName) => {
   const commandNameToUrl = {
     eBible: "https://ebible.org/study/",
     BlueLetterBible: "https://www.blueletterbible.org/",
-    TSK: "https://tsk-online.com/TSK/",
+    InstantBible: "https://instant.bible/",
     BollsLife: "https://bolls.life/",
   };
   chrome.tabs.create({ url: commandNameToUrl?.[commandName] });
